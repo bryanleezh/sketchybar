262cb3aa98949a94935ae7a2c43b6740f3599e46
Bryan Lee <leebryan307@gmail.com>
2026-05-02 17:15:12 +0800

    fix media player for macOS Sequoia 15.4

--- items/media.sh
+++ items/media.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sketchybar --add item media right \
+SPOTIFY_EVENT="com.spotify.client.PlaybackStateChanged"
+
+sketchybar --add event spotify_change $SPOTIFY_EVENT \
+           --add item media right \
            --set media label.color=$BAR_COLOR \
                        label.max_chars=20 \
                        scroll_texts=on \
@@ -8,4 +11,4 @@ sketchybar --add item media right \
                        icon.color=$BAR_COLOR   \
                        background.color=$ROSEWATER \
                        script="$PLUGIN_DIR/media.sh" \
-           --subscribe media media_change
+           --subscribe media spotify_change
--- plugins/media.sh
+++ plugins/media.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-STATE="$(echo "$INFO" | jq -r '.state')"
-if [ "$STATE" = "playing" ]; then
-  MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
-  sketchybar --set $NAME label="$MEDIA" drawing=on
+STATE="$(echo "$INFO" | jq -r '.["Player State"] // empty')"
+
+if [ "$STATE" = "Playing" ]; then
+  TRACK="$(echo "$INFO" | jq -r '.Name // empty')"
+  ARTIST="$(echo "$INFO" | jq -r '.Artist // empty')"
+  if [ -n "$ARTIST" ]; then
+    MEDIA="$TRACK - $ARTIST"
+  else
+    MEDIA="$TRACK"
+  fi
+  sketchybar --set "$NAME" label="$MEDIA" drawing=on
 else
-  # sketchybar --set $NAME drawing=off
-  sketchybar --set $NAME label="No Media"
+  sketchybar --set "$NAME" label="No Media"
 fi
